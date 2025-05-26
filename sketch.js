6f3c100d8a7440e0c727f597a2a4e69172279dcc
selina950319 <qwer950319@gmail.com>
2025-05-26 10:03:38 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,101 +30,75 @@ function draw() {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
-    // 紅色臉部輪廓線
-    stroke('red');
-    strokeWeight(15);
-    noFill();
-    beginShape();
-    let faceIndexes1 = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
-    for (let i of faceIndexes1) {
-      let [x, y] = keypoints[i];
-      vertex(x, y);
-    }
-    endShape();
-
-    // 鼻子圓圈
-    const nose = keypoints[4];
-    fill(255, 0, 0);
-    noStroke();
-    ellipse(nose[0], nose[1], 50, 50);
-  }
-}
-
-
-function drawFace() {
-  for (let prediction of predictions) {
-    const keypoints = prediction.scaledMesh;
-
-    // 第一組紅色線條
+    // 紅色臉部輪廓
     stroke('red');
     strokeWeight(15);
     noFill();
     beginShape();
     for (let i of faceIndexes1) {
-      const [x, y] = keypoints[i];
-      vertex(x, y);
+      if (i < keypoints.length) {
+        const [x, y] = keypoints[i];
+        vertex(x, y);
+      }
     }
     endShape();
 
-        // 黃色填色區域
-        fill('yellow');
-        stroke('red');
-        strokeWeight(1);
-        beginShape();
-        let faceIndexes2 = [76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184];
-        for (let i of faceIndexes2) {
-          let [x, y] = keypoints[i];
-          vertex(x, y);
-        }
-        endShape(CLOSE);
-    
-        // 綠色中間區域
-        fill('green');
-        beginShape();
-        let allPoints = faceIndexes1.concat(faceIndexes2);
-        for (let i of allPoints) {
-          let [x, y] = keypoints[i];
-          vertex(x, y);
-        }
-        endShape(CLOSE);
-    
-    // 中間綠色區域
-    fill('green');
-    beginShape();
-    for (let i of faceIndexes1.concat(faceIndexes2)) {
-      const [x, y] = keypoints[i];
-      vertex(x, y);
-    }
-    endShape(CLOSE);
+    // 黃色區域
+    drawFilledArea(faceIndexes2, 'yellow');
 
+    // 綠色中間區域
+    drawFilledArea(faceIndexes1.concat(faceIndexes2), 'green');
+
+    // 根據手勢移動圓圈
+    detectHandGesture();
     if (!circlePos) {
-      // 預設放鼻子，通常是點 1 或 4（選 1）
-      const [x, y] = keypoints[1];
+      const [x, y] = keypoints[4]; // 預設鼻子
       circlePos = createVector(x, y);
     } else {
-      // 根據手勢移動圓圈位置
       if (currentGesture === 'rock') {
-        let [x, y] = keypoints[10]; // 額頭（點10）
+        const [x, y] = keypoints[10]; // 額頭
         circlePos.set(x, y);
       } else if (currentGesture === 'paper') {
-        let [x, y] = keypoints[33]; // 左眼
+        const [x, y] = keypoints[33]; // 左眼
         circlePos.set(x, y);
       } else if (currentGesture === 'scissors') {
-        let [x, y] = keypoints[234]; // 左臉頰
+        const [x, y] = keypoints[234]; // 左臉頰
         circlePos.set(x, y);
       }
     }
+
+    drawCircle();
   }
 }
 
+// 畫圓圈
 function drawCircle() {
   if (circlePos) {
     noStroke();
     fill(255, 0, 0, 150);
-    ellipse(circlePos.x, circlePos.y, 50);
+    ellipse(circlePos.x, circlePos.y, 50, 50);
   }
 }
 
+// 畫指定區域
+function drawFilledArea(indexArray, colorFill) {
+  if (predictions.length === 0) return;
+  const keypoints = predictions[0].scaledMesh;
+
+  fill(colorFill);
+  stroke('red');
+  strokeWeight(1);
+  beginShape();
+  for (let i of indexArray) {
+    if (i < keypoints.length) {
+      const [x, y] = keypoints[i];
+      vertex(x, y);
+    }
+  }
+  endShape(CLOSE);
+}
+
+// 偵測手勢：拳頭=rock，張開=paper，2指=scissors
 function detectHandGesture() {
   if (handPredictions.length > 0) {
     const hand = handPredictions[0];
@@ -135,10 +109,9 @@ function detectHandGesture() {
     for (let tip of tips) {
       const tipPos = fingers[tip][3];
       const basePos = fingers[tip][0];
-      if (tipPos[1] < basePos[1]) extended++; // 判斷是否伸直
+      if (tipPos[1] < basePos[1]) extended++;
     }
 
-    // 粗略判斷手勢
     if (extended === 0) {
       currentGesture = 'rock';
     } else if (extended === 4) {
@@ -148,5 +121,7 @@ function detectHandGesture() {
     } else {
       currentGesture = 'none';
     }
+
+    console.log('偵測到手勢：', currentGesture);
   }
 }
